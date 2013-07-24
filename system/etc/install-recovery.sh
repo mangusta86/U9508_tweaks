f035ae71ef3f1f31d7072115df6c0436584aa238
Marco Fassi <marco@Aspire-5742G.(none)>
2013-07-24 18:43:24 +0100

    first commit

---
+++ system/etc/install-recovery.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+/system/bin/sysinit
+
+# Init.d support
+# Ryuinferno @ XDA 2012
+run-parts /system/etc/init.d/
+
---
+++ system/xbin/ext/efs-backup.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+
+BB=/system/bin/busybox
+
+if [ ! -f /data/.siyah/modembackup.tar.gz ]; then
+	$BB mkdir /data/.siyah;
+	$BB chmod 777 /data/.siyah;
+	$BB tar zcvf /data/.siyah/modembackup.tar.gz /modem;
+	$BB cat /dev/block/mmcblk0p3 > /data/.siyah/modemdev-mmcblk0p3.img;
+	$BB gzip /data/.siyah/modemdev-mmcblk0p3.img;
+	$BB cp /data/.siyah/modem* /data/share/;
+	$BB chmod 777 /data/share/modemdev-mmcblk0p3.img;
+	$BB chmod 777 /data/share/modembackup.tar.gz;
+	(
+		sleep 120;
+		cp /data/share/modem* /sdcard/;
+	)&
+fi;
